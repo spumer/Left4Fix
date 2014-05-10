a66a244a78125f72792be528aca3f1906e50cf57
spumer <spumer-tm@yandex.ru>
2014-05-10 14:49:15 +0600

    New version is 1.1.0.0

--- src/sdk/smsdk_config.h
+++ src/sdk/smsdk_config.h
@@ -43,7 +43,7 @@
 /* Basic information exposed publicly */
 #define SMEXT_CONF_NAME			"Left4Fix"
 #define SMEXT_CONF_DESCRIPTION	"Fix score completion on servers have 8+ players"
-#define SMEXT_CONF_VERSION		QUOTE(1.0.0.8 (TEAM_SIZE players))
+#define SMEXT_CONF_VERSION		QUOTE(1.1.0.0 (TEAM_SIZE players))
 #define SMEXT_CONF_AUTHOR		"spumer"
 #define SMEXT_CONF_URL			"https://forums.alliedmods.net/member.php?u=151387"
 #define SMEXT_CONF_LOGTAG		"L4FIX"
